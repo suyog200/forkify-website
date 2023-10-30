eb60b46a2fe301e7882f8c0ee4e7cc2547575a7c
suyog200 <suygchari@gmail.com>
2023-10-30 20:01:32 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7498c48b-18b1-4909-b670-37b26ef05600';
+export const MODAL_CLOSE_SEC = 2.5;
